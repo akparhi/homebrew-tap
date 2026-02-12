d75eca6b660ac938b29432e593a165352112d0d8
Anand Kumar Parhi <akp11@iitbbs.ac.in>
2026-02-12 04:59:11 +0000

    Update darkclouds to 0.5.0

--- Formula/darkclouds.rb
+++ Formula/darkclouds.rb
@@ -1,16 +1,16 @@
 class Darkclouds < Formula
   desc "Automated bug-fixing system powered by Claude"
   homepage "https://github.com/akparhi/darkclouds"
-  version "0.4.0"
+  version "0.5.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/akparhi/homebrew-tap/releases/download/v0.4.0/darkclouds-darwin-arm64.tar.gz"
-      sha256 "655dc881aef95e2d7aac228fc709e2e822518e670c67d4c6f04aacd850aeb226"
+      url "https://github.com/akparhi/homebrew-tap/releases/download/v0.5.0/darkclouds-darwin-arm64.tar.gz"
+      sha256 "84ab28e540d9cbec2e503b116167840ebe8011fa56fbc9c0c73daec63e461dc6"
     else
-      url "https://github.com/akparhi/homebrew-tap/releases/download/v0.4.0/darkclouds-darwin-x64.tar.gz"
-      sha256 "8bdf90c5acd4de598e1bd479efc253326a64957513bf72bc3c7a742e13906628"
+      url "https://github.com/akparhi/homebrew-tap/releases/download/v0.5.0/darkclouds-darwin-x64.tar.gz"
+      sha256 "b947f7ab228f5f7982eed6b39504100dbfe3244b7092a21fd34cf2d6b6e4ef5f"
     end
   end
 
