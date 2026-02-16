af16cfe81f498685b03cb63e9ca9124615f68937
Anand Kumar Parhi <akp11@iitbbs.ac.in>
2026-02-16 06:59:27 +0000

    Update ferrix to 0.2.7

--- Formula/ferrix.rb
+++ Formula/ferrix.rb
@@ -1,20 +1,18 @@
 class Ferrix < Formula
   desc "Automated bug-fixing system powered by Claude"
   homepage "https://github.com/akparhi/darkclouds"
-  version "0.2.6"
+  version "0.2.7"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/akparhi/homebrew-tap/releases/download/v0.2.6/ferrix-darwin-arm64.tar.gz"
-      sha256 "cb582a8b6a3f5e26e68d071b551ec661e6a8f5c4ce64baa25e51b382b7af0a05"
+      url "https://github.com/akparhi/homebrew-tap/releases/download/v0.2.7/ferrix-darwin-arm64.tar.gz"
+      sha256 "904b51495d0a04522f97f3bf81bf695a77193c6297c6210dfeb498ba33047554"
     else
-      url "https://github.com/akparhi/homebrew-tap/releases/download/v0.2.6/ferrix-darwin-x64.tar.gz"
-      sha256 "32ddb03fad785a041faaf11f455b6bd2373942ce6e04b6a837cf403e39905341"
+      url "https://github.com/akparhi/homebrew-tap/releases/download/v0.2.7/ferrix-darwin-x64.tar.gz"
+      sha256 "828de48904f6fc43d7abd83013937af758017a32975126f91957d1337dd2ca46"
     end
   end
-
-  depends_on cask: "font-jetbrains-mono-nerd-font"
 
   def install
     bin.install "ferrix"
