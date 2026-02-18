bb7bb50c832eb1a622c1fff531ff182f1e789a62
Anand Kumar Parhi <akp11@iitbbs.ac.in>
2026-02-18 12:59:44 +0000

    Update ferrix to 0.4.2

--- Formula/ferrix.rb
+++ Formula/ferrix.rb
@@ -1,16 +1,16 @@
 class Ferrix < Formula
   desc "Automated bug-fixing system powered by Claude"
   homepage "https://github.com/akparhi/darkclouds"
-  version "0.4.1"
+  version "0.4.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/akparhi/homebrew-tap/releases/download/v0.4.1/ferrix-darwin-arm64.tar.gz"
-      sha256 "0c700bb911f613854b0e6b72ef3b8b8e72c3457ec9c170337867d9b1076b38cb"
+      url "https://github.com/akparhi/homebrew-tap/releases/download/v0.4.2/ferrix-darwin-arm64.tar.gz"
+      sha256 "8c6d489cb5de44d02acd96448535ef89ea01c531c2ef07ae14a598f9b73f5f0c"
     else
-      url "https://github.com/akparhi/homebrew-tap/releases/download/v0.4.1/ferrix-darwin-x64.tar.gz"
-      sha256 "6f7224d0691a2b7250a1279e1e3205dc28e6401c902a491ac5259433902c0f1c"
+      url "https://github.com/akparhi/homebrew-tap/releases/download/v0.4.2/ferrix-darwin-x64.tar.gz"
+      sha256 "83085a49404e2aa657240a1b7d422d9ad03c478a73133b85d223233e4ba686c4"
     end
   end
 
