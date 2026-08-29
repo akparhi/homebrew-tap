10296c73e367d568194d5f270948a7c34adc49c9
Anand Kumar Parhi <akp11@iitbbs.ac.in>
2026-08-29 09:46:55 +0000

    Update revan to 0.1.4

--- Formula/revan.rb
+++ Formula/revan.rb
@@ -1,16 +1,16 @@
 class Revan < Formula
   desc "Routes Codex and OpenRouter models to Claude Code"
   homepage "https://github.com/akparhi/revan"
-  version "0.1.3"
+  version "0.1.4"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/akparhi/revan/releases/download/v0.1.3/revan-darwin-arm64.tar.gz"
-      sha256 "de76b8d8a30f2653f69707a1efca8dfd3342aec4210dff41d5fd3f0d392f5d34"
+      url "https://github.com/akparhi/revan/releases/download/v0.1.4/revan-darwin-arm64.tar.gz"
+      sha256 "5e016b8e46a7ce7100b836a16849445e52b1113b6580014b51024d0bbd58af62"
     else
-      url "https://github.com/akparhi/revan/releases/download/v0.1.3/revan-darwin-x64.tar.gz"
-      sha256 "4f3765db4b087fd2a15f2d661fe569fb9e231fe13f57da49ee0b99679e88b811"
+      url "https://github.com/akparhi/revan/releases/download/v0.1.4/revan-darwin-x64.tar.gz"
+      sha256 "63e96e29b8ed5884a9f8cdb0ab7f8a1e356af599494f7135460e1156d6e1b260"
     end
   end
 
